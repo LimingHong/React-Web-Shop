08937dc1d0f704ef75f0dd9c616b7137015900a2
Liming Hong <lhong@ualberta.ca>
2022-12-23 11:17:02 -0700

    Add Routes and Navigation
    Add Routes and Navigation

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
-import './categories.styles.scss'
-import Directory from './components/directory/directory.component';
-const App = () => {
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
-  return (
-    <Directory categories={categories}></Directory>    
-  );
+import Home from './routes/home/home.component'
+import Navigation from './routes/navigation/navigation.component';
+import { Routes, Route} from 'react-router-dom';
+const App = () => { 
+  return(
+  <Routes>
+    <Route path = '/' element ={<Navigation/>}>
+      <Route index element={<Home/>}/>
+      <Route path='shop' element={<Shop/>}/>                 
+    </Route> 
+    
+  </Routes>
+  
+  )
+}
+// <Rounte index make the component as default show in the parent route
+const Shop =()=>{
+  return(
+    <h1>I am the shop Page</h1>
+
+  )
 }
 
+
+//This Rountes works because App was wrapped by <BrowserRouter></BrowserRouter>
 export default App;
